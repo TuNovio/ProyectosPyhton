921305004d7b3f147c1be121a5148058e5f07373
Alfonso Gomajoa <Alfonso.alfgomachi@gmail.com>
2021-10-26 07:49:21 -0500

    manejo de funciones decoradoras

---
+++ Decoradores/decorador.py
@@ -0,0 +1,14 @@
+""" 
+Un decorador es una función para extender la funcionalidad 
+"""
+def funcionDecoradorA(funcionDecoradorB):
+    def funcionDecoradoraC():
+        funcionDecoradorB()
+    return funcionDecoradoraC
+     
+@funcionDecoradorA
+def monstrarMensaje():
+    print("Hola desde la funcion mostrar mensaje")
+    
+    
+monstrarMensaje()
