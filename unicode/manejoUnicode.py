9081fbc212b730d3c18ebfafdec2d670d22fd4b6
Alfonso Gomajoa <Alfonso.alfgomachi@gmail.com>
2021-10-26 07:28:14 -0500

    Manejo de simbolos, colores, imagenes en unicode

---
+++ unicode/manejoUnicode.py
@@ -0,0 +1,8 @@
+#salto de linea
+print("hola\u0020mundo")
+#mano que señala algo
+print("flecha \u261E mia")
+#corazon
+print("mi corazon \u2766")
+#titulo en texto 
+print("Dijo:\u261E 'nos perdimos'")
