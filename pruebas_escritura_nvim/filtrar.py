934fa4051907863674568849474bc3daccdd0850
tuNovio <alfgomachi@gmail.com>
2021-12-14 01:02:14 -0500

    se aumenta el rango para filtarar las palabras si la balabra es < al rango entonces no se imprime esa palabra

--- pruebas_escritura_nvim/filtrar.py
+++ pruebas_escritura_nvim/filtrar.py
@@ -2,11 +2,11 @@ def filtrar(palabras, cont):
     mensaje = ""
     for l in palabras:
         t = len(l)
-        if(t > cont):
+        if(t >= cont):
             mensaje += f"\n {l}"
     return mensaje
 
-lista = ["lunes", "alfonso", "martes", "jupyter", "miercoles", "hercules", "ald"]
-con = 5
+lista = ["lunes", "alfonso", "martes", "jupyter", "miercoles", "hercules", "ald", "sebastian", "jesusAlfonso"]
+con = 8
 print(filtrar(lista, con))
  
